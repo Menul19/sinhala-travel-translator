414ce2a3f5ec08eeed92b69c7cbc242190ecfbfa
Menul19 <menulmihisara2007@gmail.com>
2026-01-02 19:06:04 +0530

    Update tanslation_service.dart

--- lib/service/tanslation_service.dart
+++ lib/service/tanslation_service.dart
@@ -1,1 +1,16 @@
 
+import 'package:translator/google_translator.dart';
+
+class TranslationService {
+  final _translator = GoogleTranslator();
+
+  // ඕනෑම අකුරු පෙළක් සිංහලෙන් ඉංග්‍රීසියට හෝ අනෙක් පසට හැරවීමට
+  Future<String> translateText(String input, String targetLang) async {
+    try {
+      var translation = await _translator.translate(input, to: targetLang);
+      return translation.text;
+    } catch (e) {
+      return "පරිවර්තනය අසාර්ථකයි: $e";
+    }
+  }
+}
